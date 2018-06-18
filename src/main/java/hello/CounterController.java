6eda709298b8ba3b28c97b9a1d2084ec6853a1fe
kshitijchoudha <kshitij.choudha@gmail.com>
2018-06-18 16:05:14 -0500

    First commit

---
+++ src/main/java/hello/CounterController.java
@@ -0,0 +1,32 @@
+package hello;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import io.micrometer.core.annotation.Timed;
+
+/**
+ * Controller for counter.
+ * 
+ * @author ru-rocker
+ *
+ */
+@RestController
+public class CounterController {
+
+	private AtomicInteger atomicInteger = new AtomicInteger();
+
+	@Timed(value = "get.counter.requests", histogram = true, percentiles = { 0.95, 0.99 }, extraTags = { "version",
+			"v1" })
+	@GetMapping(path = "/counter")
+	public CounterDto getCounter(@RequestParam("message") String message) {
+		CounterDto dto = new CounterDto();
+		dto.setMessage(message);
+		dto.setCounter(atomicInteger.incrementAndGet());
+		dto.setTimestamp(System.currentTimeMillis());
+		return dto;
+	}
+}
