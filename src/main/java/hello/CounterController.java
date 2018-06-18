569963b53688085af1a7ab0e4ebc908cbda47423
kshitijchoudha <kshitij.choudha@gmail.com>
2018-06-18 17:17:53 -0500

    Print Logs

--- src/main/java/hello/CounterController.java
+++ src/main/java/hello/CounterController.java
@@ -1,10 +1,12 @@
 package hello;
+
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import io.micrometer.core.annotation.Timed;
 
@@ -18,6 +20,7 @@ import io.micrometer.core.annotation.Timed;
 public class CounterController {
 
 	private AtomicInteger atomicInteger = new AtomicInteger();
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Timed(value = "get.counter.requests", histogram = true, percentiles = { 0.95, 0.99 }, extraTags = { "version",
 			"v1" })
@@ -27,6 +30,19 @@ public class CounterController {
 		dto.setMessage(message);
 		dto.setCounter(atomicInteger.incrementAndGet());
 		dto.setTimestamp(System.currentTimeMillis());
+		logger.trace("This is a trace message");
 		return dto;
 	}
+
+	@Timed(value = "get.log.prints", histogram = true, percentiles = { 0.95, 0.99 }, extraTags = { "version",
+			"v1" })
+	@GetMapping(path = "/printLogs")
+	public String printSomeLogs() {
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.warn("This is a warn message");
+		logger.error("This is an error message");
+		return "Did some logging..";
+	}
+
 }
